1c6330d7af4dc20f8d6d4e852703c068d8f1f8b4
iGhibli <iGhibli@163.com>
2016-02-25 20:11:43 +0800

    完成基本框架搭建

---
+++ dodo/dodo/ContactsVC.h
@@ -0,0 +1,13 @@
+//
+//  Contacts.h
+//  dodo
+//
+//  Created by qingyun on 16/2/25.
+//  Copyright © 2016年 iGhibli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsVC : UITableViewController
+
+@end
---
+++ dodo/dodo/ConversationVC.h
@@ -0,0 +1,13 @@
+//
+//  ConversationVC.h
+//  dodo
+//
+//  Created by qingyun on 16/2/25.
+//  Copyright © 2016年 iGhibli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConversationVC : UITableViewController
+
+@end
